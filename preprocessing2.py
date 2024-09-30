6058a3f15f74833c82e394dba9e2e7d364209512
Prasun  Dandapat <129579131+5Prasun@users.noreply.github.com>
2024-09-30 12:34:49 +0530

    hello

---
+++ preprocessing2.py
@@ -0,0 +1,51 @@
+#Introduction to Preprocessing in AI-ML
+
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
+
+
+#Importing the dataset
+dataset=pd.read_csv('Data.csv')
+
+x=dataset.iloc[:,:-1].values
+y=dataset.iloc[:,-1].values
+
+#print(x)
+#print(y)
+
+#Taking care of the missing data in dataset
+from sklearn.impute import SimpleImputer
+imputer=SimpleImputer(missing_values=np.nan,strategy='mean');
+imputer.fit(x[:,1:3])
+
+x[:,1:3]=imputer.transform(x[:,1:3])
+
+#print(x)
+
+#Encoding the categorical data from the dataset
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct=ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[0])],remainder="passthrough")
+x=np.array(ct.fit_transform(x))
+
+print(x)
+
+#encoding for the dependent variable
+from sklearn.preprocessing import LabelEncoder
+le=LabelEncoder()
+y=le.fit_transform(y)
+
+print(y)
+
+#spilt the data into training set and test set
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=1)
+
+print(x_train)
+print(x_test)
+print(y_train)
+print(y_test)
