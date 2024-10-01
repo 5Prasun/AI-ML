87f91fc6735784d14a9006c1e56d0f4bf9d86dd4
Prasun  Dandapat <129579131+5Prasun@users.noreply.github.com>
2024-10-01 09:59:53 +0530

    hii

--- preprocessing.py
+++ preprocessing.py
@@ -45,7 +45,17 @@ print(y)
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=1)
 
+#print(x_train)
+#print(x_test)
+#print(y_train)
+#print(y_test)
+
+
+# Features Scaling in Data Preprocessing
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+x_train[:,3:]=sc.fit_transform(x_train[:,3:])
+x_test[:,3:]=sc.transform(x_test[:,3:])
+
 print(x_train)
 print(x_test)
-print(y_train)
-print(y_test)
